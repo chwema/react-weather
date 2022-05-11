7fcb902f597a905eb60db28d87d049a3a4cb4d57
chwema <101362399+chwema@users.noreply.github.com>
2022-05-11 10:29:47 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/chido-chimbetete-5291bb177/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Chido Chimbetete
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/chwema/weather-react"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/chido-chimbetete-5291bb177/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Chido Chimbetete
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/chwema/weather-react"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
