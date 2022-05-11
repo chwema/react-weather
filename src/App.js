560b8c664731ccced489783036d983542eb48f56
chwema <101362399+chwema@users.noreply.github.com>
2022-05-11 10:08:49 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Chido Chimbetete and is
+        <a
+          href="https://github.com/chwema/weather-react"
+          rel="noreferrer"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
